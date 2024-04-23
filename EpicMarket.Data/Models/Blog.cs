ed3f5c624cb1917602a7d1f5e3d706893b3dd3a0
Gadamsetti Akhil <50011984+gadamsatti@users.noreply.github.com>
2024-04-23 23:40:31 +0530

    Create Blog,Faq,FaqCategory Models

---
+++ EpicMarket.Data/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using EpicMarket.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicMarket.Data.Models
+{
+    public class Blog : BaseModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string InnerHtml { get; set; }
+        public string Authour { get; set; }
+    }
+}
---
+++ EpicMarket.Data/Models/FAQCategory.cs
@@ -0,0 +1,18 @@
+﻿using EpicMarket.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpicMarket.Data.Models
+{
+    public class FAQCategory : BaseModel
+    {
+        public int Id { get; set; }
+        public string CategoryTitle { get; set; }
+        public string TypeOfFAQ { get; set; } //for customer,Business
+
+        public virtual ICollection<FAQ>? FAQs { get; set; }
+    }
+}
