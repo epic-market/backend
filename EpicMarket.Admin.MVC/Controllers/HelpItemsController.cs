1d43cffe8f7d77ce41cd834c36cb515f03e9d8a8
Gadamsetti Akhil <50011984+gadamsatti@users.noreply.github.com>
2024-09-03 10:36:07 +0530

    Notifcation , onbiarding

---
+++ EpicMarket.Admin.MVC/Controllers/HelpItemsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EpicMarket.Admin.MVC.Data;
+using EpicMarket.Data.Models;
+using System.Reflection.Metadata;
+using System.Security.Claims;
+
+namespace EpicMarket.Admin.MVC.Controllers
+{
+    public class HelpItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HelpItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HelpItems
+        public async Task<IActionResult> Index()
+        {
+            var authDbContext = _context.HelpItems.Include(h => h.Pages);
+            return View(await authDbContext.ToListAsync());
+        }
+
+        // GET: HelpItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var helpItem = await _context.HelpItems
+                .Include(h => h.Pages)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (helpItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(helpItem);
+        }
+
+        // GET: HelpItems/Create
+        public IActionResult Create()
+        {
+            ViewData["PageID"] = new SelectList(_context.Pages, "ID", "Name");
+            return View();
+        }
+
+        // POST: HelpItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Title,Description,PageID,IsShownOnPage,CreateDate,CreateBy,ModifiedDate,ModifiedBy,IsActive")] HelpItem helpItem)
+        {
+            var userName = this.User.FindFirst(ClaimTypes.Name).Value;
+            helpItem.CreateBy = userName;
+            helpItem.CreateDate = DateTime.UtcNow;
+            if (ModelState.IsValid)
+            {
+                _context.Add(helpItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PageID"] = new SelectList(_context.Pages, "ID", "Name", helpItem.PageID);
+            return View(helpItem);
+        }
+
+        // GET: HelpItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var helpItem = await _context.HelpItems.FindAsync(id);
+            if (helpItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["PageID"] = new SelectList(_context.Pages, "ID", "Name", helpItem.PageID);
+            return View(helpItem);
+        }
+
+        // POST: HelpItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Title,Description,PageID,IsShownOnPage,CreateDate,CreateBy,ModifiedDate,ModifiedBy,IsActive")] HelpItem helpItem)
+        {
+            var userName = this.User.FindFirst(ClaimTypes.Name).Value;
+            helpItem.ModifiedBy = userName;
+            helpItem.ModifiedDate = DateTime.UtcNow;
+            if (id != helpItem.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(helpItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HelpItemExists(helpItem.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PageID"] = new SelectList(_context.Pages, "ID", "Name", helpItem.PageID);
+            return View(helpItem);
+        }
+
+        // GET: HelpItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var helpItem = await _context.HelpItems
+                .Include(h => h.Pages)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (helpItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(helpItem);
+        }
+
+        // POST: HelpItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var helpItem = await _context.HelpItems.FindAsync(id);
+            if (helpItem != null)
+            {
+                _context.HelpItems.Remove(helpItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HelpItemExists(int id)
+        {
+            return _context.HelpItems.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ EpicMarket.Admin.MVC/Controllers/UserOnboardingProgressesController.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EpicMarket.Admin.MVC.Data;
+using EpicMarket.Data.Models;
+
+namespace EpicMarket.Admin.MVC.Controllers
+{
+    public class UserOnboardingProgressesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserOnboardingProgressesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserOnboardingProgresses
+        public async Task<IActionResult> Index()
+        {
+            var authDbContext = _context.UserOnboardingProgresses.Include(u => u.Step).Include(u => u.User);
+            return View(await authDbContext.ToListAsync());
+        }
+
+        // GET: UserOnboardingProgresses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userOnboardingProgress = await _context.UserOnboardingProgresses
+                .Include(u => u.Step)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userOnboardingProgress == null)
+            {
+                return NotFound();
+            }
+
+            return View(userOnboardingProgress);
+        }
+
+        // GET: UserOnboardingProgresses/Create
+        public IActionResult Create()
+        {
+            ViewData["StepID"] = new SelectList(_context.OnboardingSteps, "Id", "StepName");
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: UserOnboardingProgresses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserID,StepID,CompletedAt,IsCompleted")] UserOnboardingProgress userOnboardingProgress)
+        {
+
+
+            if (userOnboardingProgress.IsCompleted)
+            { 
+                userOnboardingProgress.CompletedAt = DateTime.Now;
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(userOnboardingProgress);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StepID"] = new SelectList(_context.OnboardingSteps, "Id", "StepName", userOnboardingProgress.StepID);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", userOnboardingProgress.UserID);
+            return View(userOnboardingProgress);
+        }
+
+        // GET: UserOnboardingProgresses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userOnboardingProgress = await _context.UserOnboardingProgresses.FindAsync(id);
+            if (userOnboardingProgress == null)
+            {
+                return NotFound();
+            }
+            ViewData["StepID"] = new SelectList(_context.OnboardingSteps, "Id", "StepName", userOnboardingProgress.StepID);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", userOnboardingProgress.UserID);
+            return View(userOnboardingProgress);
+        }
+
+        // POST: UserOnboardingProgresses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserID,StepID,CompletedAt,IsCompleted")] UserOnboardingProgress userOnboardingProgress)
+        {
+
+
+            if (userOnboardingProgress.IsCompleted)
+            {
+                userOnboardingProgress.CompletedAt = DateTime.Now;
+            }
+
+            if (id != userOnboardingProgress.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userOnboardingProgress);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserOnboardingProgressExists(userOnboardingProgress.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StepID"] = new SelectList(_context.OnboardingSteps, "Id", "StepName", userOnboardingProgress.StepID);
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", userOnboardingProgress.UserID);
+            return View(userOnboardingProgress);
+        }
+
+        // GET: UserOnboardingProgresses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userOnboardingProgress = await _context.UserOnboardingProgresses
+                .Include(u => u.Step)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userOnboardingProgress == null)
+            {
+                return NotFound();
+            }
+
+            return View(userOnboardingProgress);
+        }
+
+        // POST: UserOnboardingProgresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userOnboardingProgress = await _context.UserOnboardingProgresses.FindAsync(id);
+            if (userOnboardingProgress != null)
+            {
+                _context.UserOnboardingProgresses.Remove(userOnboardingProgress);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserOnboardingProgressExists(int id)
+        {
+            return _context.UserOnboardingProgresses.Any(e => e.Id == id);
+        }
+    }
+}
