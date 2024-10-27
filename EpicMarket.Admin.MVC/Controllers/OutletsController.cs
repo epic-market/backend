9dc44df25772f8b7025d4755622128655a06bf1b
Gadamsetti Akhil <50011984+gadamsatti@users.noreply.github.com>
2024-10-27 16:19:34 +0530

    Outlet Add edit Images

--- EpicMarket.Admin.MVC/Controllers/OutletsController.cs
+++ EpicMarket.Admin.MVC/Controllers/OutletsController.cs
@@ -11,6 +11,7 @@ using EpicMarket.Admin.MVC.Models;
 using System.Security.Claims;
 using EpicMarket.Entities.CustomModels;
 using Microsoft.CodeAnalysis.Operations;
+using EpicMarket.Admin.MVC.Contracts;
 
 namespace EpicMarket.Admin.MVC.Controllers
 {
@@ -18,10 +19,14 @@ namespace EpicMarket.Admin.MVC.Controllers
     public class OutletsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IAttachmentService attachmentService;
+        private readonly IFileService fileService;
 
-        public OutletsController(ApplicationDbContext context)
+        public OutletsController(ApplicationDbContext context, IAttachmentService attachmentService, IFileService fileService)
         {
             _context = context;
+            this.attachmentService = attachmentService;
+            this.fileService = fileService;
         }
 
         // GET: Outlets
@@ -110,7 +115,7 @@ namespace EpicMarket.Admin.MVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,BussinessID,AddressID,Name,Description,ContactNumber,ContactEmail,Rating,ReviewCount,IsOpen,Weight,StatusId,CreateDate,CreateBy,ModifiedDate,ModifiedBy,Address")] Outlet outlet)
+        public async Task<IActionResult> Create([Bind("ID,BussinessID,AddressID,Name,Description,ContactNumber,ContactEmail,Rating,ReviewCount,IsOpen,Weight,StatusId,CreateDate,CreateBy,ModifiedDate,ModifiedBy,Address")] Outlet outlet, IFormFile[] thumbnail, IFormFile[] OutletImages)
         {
 
             var userName = this.User.FindFirst(ClaimTypes.Name).Value;
@@ -124,6 +129,35 @@ namespace EpicMarket.Admin.MVC.Controllers
             {
                 _context.Add(outlet);
                 await _context.SaveChangesAsync();
+                if (thumbnail?.Length > 0)
+                {
+                    var attachmentThumbnail = new AttachmentModel()
+                    {
+                        Name = EntityConstants.Branch,
+                        Comment = EntityConstants.Branch,
+                        RecordID = outlet.ID,
+                        Entity = EntityConstants.Branch,
+                        AttachmentType = AttachmentTypeConstants.BRANCH_THUMBNAIL,
+                        FolderPathConstant = FilePathConstants.BranchThumbnail,
+                        Files = thumbnail
+                    };
+                    await attachmentService.InsertAttachment(attachmentThumbnail);
+                }
+
+                if (OutletImages?.Length > 0)
+                {
+                    var attachmentOutletImages = new AttachmentModel()
+                    {
+                        Name = EntityConstants.Branch,
+                        Comment = EntityConstants.Branch,
+                        RecordID = outlet.ID,
+                        Entity = EntityConstants.Branch,
+                        AttachmentType = AttachmentTypeConstants.BRANCH_PHOTOS,
+                        FolderPathConstant = FilePathConstants.BranchesPhotos,
+                        Files = OutletImages
+                    };
+                    await attachmentService.InsertAttachment(attachmentOutletImages);
+                }
                 return RedirectToAction(nameof(Index));
             }
             ViewData["AddressID"] = new SelectList(_context.Addresses, "Id", "Id", outlet.AddressID);
@@ -142,6 +176,19 @@ namespace EpicMarket.Admin.MVC.Controllers
             }
 
             var outlet = await _context.Outlets.Where(b => b.ID == id).Include(c => c.Address).Include(c => c.StatusOptionSets).FirstOrDefaultAsync();
+            ViewBag.thumbnails = await this.attachmentService.GetAttachmentLinks(new GetAttachmentLink()
+            {
+                AttachmentType = AttachmentTypeConstants.BRANCH_THUMBNAIL,
+                Entity = EntityConstants.Branch,
+                RecordID = outlet.ID
+            });
+
+            ViewBag.BranchImages = await this.attachmentService.GetAttachmentLinks(new GetAttachmentLink()
+            {
+                AttachmentType = AttachmentTypeConstants.BRANCH_PHOTOS,
+                Entity = EntityConstants.Branch,
+                RecordID = outlet.ID
+            });
             if (outlet == null)
             {
                 return NotFound();
@@ -157,7 +204,7 @@ namespace EpicMarket.Admin.MVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,BussinessID,AddressID,Name,Description,ContactNumber,ContactEmail,Rating,ReviewCount,IsOpen,Weight,StatusId,CreateDate,CreateBy,ModifiedDate,ModifiedBy,Address,IsActive")] Outlet outlet)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,BussinessID,AddressID,Name,Description,ContactNumber,ContactEmail,Rating,ReviewCount,IsOpen,Weight,StatusId,CreateDate,CreateBy,ModifiedDate,ModifiedBy,Address,IsActive")] Outlet outlet, IFormFile[] newThumbnail, string? addedThumbnail, IFormFile[] newOutletImages, string? removedOutletImages, string? addedOutletImages)
         {
 
             var userName = this.User.FindFirst(ClaimTypes.Name).Value;
@@ -180,6 +227,57 @@ namespace EpicMarket.Admin.MVC.Controllers
                 {
                     _context.Update(outlet);
                     await _context.SaveChangesAsync();
+                    if (addedThumbnail != null && addedThumbnail?.Length > 0)
+                    {
+                        List<string> ExisitingThumbnail = await this.attachmentService.GetAttachmentLinks(new GetAttachmentLink()
+                        {
+                            AttachmentType = AttachmentTypeConstants.BRANCH_THUMBNAIL,
+                            Entity = EntityConstants.Branch,
+                            RecordID = outlet.ID
+                        });
+                        if (ExisitingThumbnail?.Count > 0)
+                        {
+                            await this.fileService.DeleteImage(ExisitingThumbnail, userName);
+                        }
+
+                        var attachmentThumbnail = new AttachmentModel()
+                        {
+                            Name = EntityConstants.Branch,
+                            Comment = EntityConstants.Branch,
+                            RecordID = outlet.ID,
+                            Entity = EntityConstants.Branch,
+                            AttachmentType = AttachmentTypeConstants.BRANCH_THUMBNAIL,
+                            FolderPathConstant = FilePathConstants.BranchThumbnail,
+                            Files = newThumbnail
+                        };
+                        await attachmentService.InsertAttachment(attachmentThumbnail);
+
+                    }
+
+                    // Handle removed images
+                    if (!string.IsNullOrEmpty(removedOutletImages))
+                    {
+                        var removedImageUrls = removedOutletImages.Split(',').ToList();
+                        await this.fileService.DeleteImage(removedImageUrls, userName);
+                    }
+
+                    // Handle added images
+                    if (addedOutletImages != null && newOutletImages?.Length > 0)
+                    {
+
+                        var attachmentOutletImages = new AttachmentModel()
+                        {
+                            Name = EntityConstants.Branch,
+                            Comment = EntityConstants.Branch,
+                            RecordID = outlet.ID,
+                            Entity = EntityConstants.Branch,
+                            AttachmentType = AttachmentTypeConstants.BRANCH_PHOTOS,
+                            FolderPathConstant = FilePathConstants.BranchesPhotos,
+                            Files = newOutletImages
+                        };
+                        await attachmentService.InsertAttachment(attachmentOutletImages);
+
+                    }
                 }
                 catch (DbUpdateConcurrencyException)
                 {
